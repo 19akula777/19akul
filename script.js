d56a1199dcedaf2760c6522ce4f2bb0e8b9136f3
19akula777 <19akul@gmail.com>
2020-05-31 23:43:17 +0300

    Second lesson

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 
 	let money = prompt('Ваш бюджет на месяц?');
 		time = prompt('Введите дату в формате YYYY-MM-DD');
-		question = prompt('Введите обязательную статью расходов в этом месяце');
-		answer = prompt('Во сколько обойдется?');
 		money_day = money / 30;
 
-	alert( money_day );
 
-	appData = {
-		butzhet: money,
+	let appData = {
+		budget: money,
 		timeData: time,
-		expenses: {
-		    question : answer
-		    },
+		expenses: {},
 	    optionalExpenses:'' ,
 	    income:[] ,
-	    savings: false
-
+	    savings: false,
+	    money_day: money_day
 	    
 	};
 
+	for (let i = 0; i < 2; i++) {
+		let question1 = prompt('Введите обязательную статью расходов в этом месяце');
+			question2 = prompt('Во сколько обойдется?');
 
+		if ( typeof (question1) != null && question1 != '' && typeof (question2) != null && question2 != ''
+		&& typeof (question1) === "string") {
+			appData.expenses[question1] = question2;
+			
+		} else {
 
-	console.log(time);
-	console.log(money);
+		}		
+	};
+
 	console.log(appData);
+
+	switch (true){
+		case appData.money_day < 100:
+			console.log('Низкий уровень достатка');
+			break;
+		case appData.money_day >= 100 && appData.money_day <= 1000:
+			console.log('Средний уровень достатка');
+			break;
+		case appData.money_day > 1000:
+			console.log('Высокий уровень достатка');
+			break;
+	}
+
+
+
+
+
+	
 
